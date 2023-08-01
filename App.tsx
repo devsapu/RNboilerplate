1f7f0d3b284b659e195208a13cf93304a071ef7d
Sapumal Thepulangoda <sapumal.t90@gmail.com>
2023-08-01 13:18:16 +0530

    adding sign in , login screens

--- App.tsx
+++ App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Login from './src/screens/users/login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/Home';
+import LoginScreen from './src/screens/users/login';
+import SignInScreen from './src/screens/users/signIn';
+
+
+
+const Stack = createNativeStackNavigator();
+
 const App = () => {
-  const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
+
+
+
