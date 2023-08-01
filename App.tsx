a7d0c79e0988f058756699e3681a4a394257ef11
Sapumal Thepulangoda <sapumal.t90@gmail.com>
2023-08-01 13:02:47 +0530

    Login screen added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from './src/screens/users/login';
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
